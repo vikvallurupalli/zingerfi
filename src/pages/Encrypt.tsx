dcb0c33bdbbbf529c7f26cdc9ba478077ba74315
vikvallurupalli <72287969+vikvallurupalli@users.noreply.github.com>
2025-12-04 21:56:10 -0600

    Update Encrypt.tsx

--- src/pages/Encrypt.tsx
+++ src/pages/Encrypt.tsx
@@ -107,7 +107,7 @@ export default function Encrypt() {
   };
 
   const createShareLink = () => {
-    return `https://zingerfi.snitchon.org/decrypt?message=${encodeURIComponent(encryptedText)}`;
+    return `https://zinger.snitchon.org/decrypt?message=${encodeURIComponent(encryptedText)}`;
   };
 
   const handleSMS = () => {
